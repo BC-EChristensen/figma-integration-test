3da675997b49a7b7129a595c33ddbe99ead1f422
Erik Christensen <32500994+BC-EChristensen@users.noreply.github.com>
2018-09-30 11:47:10 -0700

    figma update

--- colors.ts
+++ colors.ts
@@ -1,39 +1,31 @@
 export interface Colors {
+    action: string;
     background: string;
-    backgroundHover: string;
-    brandPrimary: string;
-    empty: string;
+    brand: string;
     error: string;
     guideText: string;
-    icons: string;
     primary: string;
-    primaryHover: string;
-    primaryText: string;
-    secondaryHover: string;
-    secondaryText: string;
-    selectedBackground: string;
+    secondary: string;
+    selected: string;
     stroke: string;
     success: string;
     warning: string;
+    warningHover: string;
 }
 
-const colors: Colors = {
+const colors: Colors {
+    action: '#3F69FE',
     background: '#F5F7FA',
-    backgroundHover: '#ECEFF4',
-    brandPrimary: '#273A8A',
-    empty: '#FFFFFF',
+    brand: '#273A8A',
     error: '#F0603C',
     guideText: '#ABB1BE',
-    icons: '#5C5F66',
-    primary: '#3F69FE',
-    primaryHover: '#2044C4',
-    primaryText: '#303540',
-    secondaryHover: '#F5F7FF',
-    secondaryText: '#72767F',
-    selectedBackground: '#EDF0FE',
+    primary: '#303540',
+    secondary: '#72767F',
+    selected: '#EDF0FE',
     stroke: '#CFD6E5',
     success: '#30AB30',
     warning: '#FFD24D',
+    warningHover: '#FFDE7E',
 };
 
 export default colors;
