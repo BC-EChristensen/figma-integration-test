6fa28e05b81c0c4b6b6b025df1229ed698ba8044
Erik Christensen <erik.christensen@bigcommerce.com>
2018-09-30 10:50:57 -0700

    First commit

---
+++ colors.ts
@@ -0,0 +1,39 @@
+export interface Colors {
+    background: string;
+    backgroundHover: string;
+    brandPrimary: string;
+    empty: string;
+    error: string;
+    guideText: string;
+    icons: string;
+    primary: string;
+    primaryHover: string;
+    primaryText: string;
+    secondaryHover: string;
+    secondaryText: string;
+    selectedBackground: string;
+    stroke: string;
+    success: string;
+    warning: string;
+}
+
+const colors: Colors = {
+    background: '#F5F7FA',
+    backgroundHover: '#ECEFF4',
+    brandPrimary: '#273A8A',
+    empty: '#FFFFFF',
+    error: '#F0603C',
+    guideText: '#ABB1BE',
+    icons: '#5C5F66',
+    primary: '#3F69FE',
+    primaryHover: '#2044C4',
+    primaryText: '#303540',
+    secondaryHover: '#F5F7FF',
+    secondaryText: '#72767F',
+    selectedBackground: '#EDF0FE',
+    stroke: '#CFD6E5',
+    success: '#30AB30',
+    warning: '#FFD24D',
+};
+
+export default colors;
