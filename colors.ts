3a0637bb24f0d691cdafa478e2d50b2a5c7eed6e
Erik Christensen <32500994+BC-EChristensen@users.noreply.github.com>
2018-10-01 08:50:26 -0700

    figma update

--- colors.ts
+++ colors.ts
@@ -2,32 +2,34 @@ export interface Colors {
     action: string;
     black: string;
     brand: string;
+    brightGrey: string;
     darkGrey: string;
     dimGrey: string;
     error: string;
     grey: string;
     lightGrey: string;
-    lightGrey: string;
     selected: string;
     success: string;
     warning: string;
     warningHover: string;
+    white: string;
 }
 
 const colors: Colors {
     action: '#3F69FE',
     black: '#303540',
     brand: '#273A8F',
+    brightGrey: '#F5F7FA',
     darkGrey: '#72767F',
     dimGrey: '#ABB1BE',
     error: '#F0603C',
     grey: '#D3D9E5',
     lightGrey: '#ECEFF4',
-    lightGrey: '#ECEFF4',
     selected: '#EDF0FE',
     success: '#30AB3C',
     warning: '#FFD24D',
     warningHover: '#FFDE7F',
+    white: '#F5F7FA',
 };
 
 export default colors;
